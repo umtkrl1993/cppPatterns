068085b152d848d99b3ce6a314b427fdebd99f00
umtkrl1993 <umitaykurt1993@gmail.com>
2016-09-07 22:42:10 +0300

    SimplePizzaFactory handles pizza objects creation

---
+++ SimplePizzaFactory.h
@@ -0,0 +1,13 @@
+#ifndef SIMPLE_PIZZA_FACTORY_H
+#define SIMPLE_PIZZA_FACTORY_H
+#include"Pizza.h"
+class SimplePizzaFactory {
+public:
+   static Pizza createPizza(std::string type); 
+
+};
+
+
+
+
+#endif
